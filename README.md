0e1bc619874fb1f25892fbb5ef3be1d591ff5ded
Polina Shubina <shubina@reg.ru>
2015-02-16 15:12:53 +0300

    doc

--- README.md
+++ README.md
@@ -9,25 +9,29 @@ version 0.001
 # SYNOPSIS
 
     use Lingua::RU::Num2Word qw/num2rus_cardinal/;
-    print num2rus_cardinal(21, 2); # двадцать одно
+    print num2rus_cardinal(21, 'NEUTER'); # двадцать одно
 
 ## num2rus\_cardinal( $number, $gender )
 
 Translates number to text converter for russian, using the specified gender. Returns Unicode string.
 Main code was taken from [Lingua::RUS::Number](https://metacpan.org/pod/Lingua::RUS::Number).
 
-$gender 0|1|2 - female, male and neutral respectively. Male by default.
+$gender
+    Can be
+        FEMININE
+        MASCULINE
+        NEUTER
 
     use Lingua::RU::Num2Word qw/num2rus_cardinal/;
     my $text = num2rus_cardinal(561); # outputs пятьсот шестьдесят один
 
 
     my $bottles_on_wall = 22;
-    print num2rus_cardinal($bottles_on_wall, 0) . " бутылки пива на стене"; # outputs "двадцать две бутылки пива на стене"
+    print num2rus_cardinal($bottles_on_wall, 'FEMININE') . " бутылки пива на стене"; # outputs "двадцать две бутылки пива на стене"
     $bottles_on_wall --;
-    print num2rus_cardinal($bottles_on_wall, 0) . " бутылка пива на стене"; # outputs "двадцать одна бутылка пива на стене"
+    print num2rus_cardinal($bottles_on_wall, 'FEMININE') . " бутылка пива на стене"; # outputs "двадцать одна бутылка пива на стене"
 
-# ORIGINAL MODULE
+# ORIGINAL MODULE [Lingua::RUS::Number](https://metacpan.org/pod/Lingua::RUS::Number)
 
     fork coding, maintenance, refactoring, extensions:  Richard C. Jelinek <info@petamem.com>
     initial coding:  Vladislav A. Safronov, E<lt>F<vlads@yandex-team.ru>E<gt>, E<lt>F<vlad@yandex.ru>E<gt>
--- lib/Lingua/RU/Num2Word.pm
+++ lib/Lingua/RU/Num2Word.pm
@@ -87,9 +87,9 @@ use vars qw(%diw %nom %genders);
 );
 
 %genders = (
-    0 => { 0 => "", 1 => "", 2 => "одна", 3 => "две" },
-    1 => { 0 => "", 1 => "", 2 => "один", 3 => "два" },
-    2 => { 0 => "", 1 => "", 2 => "одно", 3 => "два" },
+    'FEMININE' => { 0 => "", 1 => "", 2 => "одна", 3 => "две" },
+    'MASCULINE' => { 0 => "", 1 => "", 2 => "один", 3 => "два" },
+    'NEUTER' => { 0 => "", 1 => "", 2 => "одно", 3 => "два" },
 );
 
 # Stolen from Lingua::RU::Number
@@ -101,23 +101,28 @@ use vars qw(%diw %nom %genders);
 Translates number to text converter for russian, using the specified gender. Returns Unicode string.
 Main code was taken from L<Lingua::RUS::Number>.
 
-$gender 0|1|2 - female, male and neutral respectively. Male by default.
+$gender
+    Can be
+        FEMININE
+        MASCULINE
+        NEUTER
 
     use Lingua::RU::Num2Word qw/num2rus_cardinal/;
     my $text = num2rus_cardinal(561); # outputs пятьсот шестьдесят один
 
 
     my $bottles_on_wall = 22;
-    print num2rus_cardinal($bottles_on_wall, 0) . " бутылки пива на стене"; # outputs "двадцать две бутылки пива на стене"
+    print num2rus_cardinal($bottles_on_wall, 'FEMININE') . " бутылки пива на стене"; # outputs "двадцать две бутылки пива на стене"
     $bottles_on_wall --;
-    print num2rus_cardinal($bottles_on_wall, 0) . " бутылка пива на стене"; # outputs "двадцать одна бутылка пива на стене"
+    print num2rus_cardinal($bottles_on_wall, 'FEMININE') . " бутылка пива на стене"; # outputs "двадцать одна бутылка пива на стене"
 
 =cut
 
 sub num2rus_cardinal {
     my ( $number, $gender ) = @_;
 
-    $gender //= 1;    # male by default
+    $gender ||= 'MASCULINE';    # masculine by default
+    $gender = uc $gender;
 
     return _get_string( 0, 0, 0 ) unless $number;    # no extra calculations for zero
 
@@ -206,11 +211,11 @@ sub _get_string {
 =head1 SYNOPSIS
 
     use Lingua::RU::Num2Word qw/num2rus_cardinal/;
-    print num2rus_cardinal(21, 2); # двадцать одно
+    print num2rus_cardinal(21, 'NEUTER'); # двадцать одно
 
 =cut
 
-=head1 ORIGINAL MODULE
+=head1 ORIGINAL MODULE L<Lingua::RUS::Number>
 
     fork coding, maintenance, refactoring, extensions:  Richard C. Jelinek <info@petamem.com>
     initial coding:  Vladislav A. Safronov, E<lt>F<vlads@yandex-team.ru>E<gt>, E<lt>F<vlad@yandex.ru>E<gt>
